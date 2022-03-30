eef456847479cbc55cb79aa795a251bbc17e95c3
Neel Shah <neelshah29042002.com>
2022-03-30 07:40:54 +0530

    tmva pythonization files added

---
+++ bindings/pyroot/pythonizations/python/ROOT/_pythonization/_tmva/_factory.py
@@ -0,0 +1,19 @@
+import ROOT
+from ROOT import TMVA
+
+from ._utils import _kwargs_to_Tmvacmdargs, cpp_signature
+
+class factory(object):
+    r"""Some member functions of factory that take a TmvaCmdArg as argument also support keyword arguments.
+    This applies to factory::BookMethod().
+
+    """
+    @cpp_signature(
+        "Factory::BookMethod( DataLoader *loader, TString theMethodName, TString methodTitle, TString theOption = "" );"
+    )
+    def BookMethod(self, *args, **kwargs):
+        r"""factory::BookMethod() function is pythonized with the command argument pythonization.
+        The keywords must correspond to the CmdArgs of the function.
+        """
+        args, kwargs = _kwargs_to_Tmvacmdargs(*args, **kwargs)
+        return self._factory(*args, **kwargs)
